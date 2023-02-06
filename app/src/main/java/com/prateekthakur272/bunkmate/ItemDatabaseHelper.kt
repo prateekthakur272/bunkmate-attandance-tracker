3c7ef0b33317c3f8887c898dba12d583be87334f
Prateek Thakur <67188426+prateekthakur272@users.noreply.github.com>
2023-02-06 11:53:10 +0530

    bug fixes

--- app/src/main/java/com/prateekthakur272/bunkmate/ItemDatabaseHelper.kt
+++ app/src/main/java/com/prateekthakur272/bunkmate/ItemDatabaseHelper.kt
@@ -10,7 +10,7 @@ class ItemDatabaseHelper(context: Context):SQLiteOpenHelper(context, DB_NAME,nul
 
     companion object{
         const val DB_NAME = "item_database.db"
-        const val TABLE_NAME = "subjects"
+        const val TABLE_ITEMS = "subjects"
         const val COLUMN_ID = "id"
         const val COLUMN_SUBJECT_NAME = "subject_name"
         const val COLUMN_ATTENDED = "lectures_attended"
@@ -19,8 +19,8 @@ class ItemDatabaseHelper(context: Context):SQLiteOpenHelper(context, DB_NAME,nul
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        val query = "create table $TABLE_NAME($COLUMN_ID integer primary key autoincrement ,$COLUMN_SUBJECT_NAME varchar(30) not null,$COLUMN_ATTENDED integer default 0,$COLUMN_TOTAL integer default 0)"
-        db?.execSQL(query)
+        val createItems = "create table $TABLE_ITEMS($COLUMN_ID integer primary key autoincrement ,$COLUMN_SUBJECT_NAME varchar(30) not null,$COLUMN_ATTENDED integer default 0,$COLUMN_TOTAL integer default 0)"
+        db?.execSQL(createItems)
         if (db!!.isOpen)
             Log.d(TAG,"Database is open")
     }
@@ -35,13 +35,13 @@ class ItemDatabaseHelper(context: Context):SQLiteOpenHelper(context, DB_NAME,nul
         cv.put(COLUMN_SUBJECT_NAME,item.title)
         cv.put(COLUMN_ATTENDED,item.lectureAttended)
         cv.put(COLUMN_TOTAL,item.totalLectures)
-        db.insert(TABLE_NAME,null,cv)
+        db.insert(TABLE_ITEMS,null,cv)
     }
     fun markAttendance(id:Int,attended:Boolean = false){
         val db = this.writableDatabase
         try {
-            val query = if (attended) "update $TABLE_NAME set $COLUMN_ATTENDED = $COLUMN_ATTENDED+1,$COLUMN_TOTAL = $COLUMN_TOTAL+1 where $COLUMN_ID = $id"
-            else "update $TABLE_NAME set $COLUMN_TOTAL = $COLUMN_TOTAL+1 where $COLUMN_ID = $id"
+            val query = if (attended) "update $TABLE_ITEMS set $COLUMN_ATTENDED = $COLUMN_ATTENDED+1,$COLUMN_TOTAL = $COLUMN_TOTAL+1 where $COLUMN_ID = $id"
+            else "update $TABLE_ITEMS set $COLUMN_TOTAL = $COLUMN_TOTAL+1 where $COLUMN_ID = $id"
             db.execSQL(query)
         }catch (e:java.lang.RuntimeException){
             Log.d(TAG,"table not found")
@@ -49,7 +49,7 @@ class ItemDatabaseHelper(context: Context):SQLiteOpenHelper(context, DB_NAME,nul
         db.close()
     }
     fun deleteItem(id: Int){
-        val query = "delete from $TABLE_NAME where $COLUMN_ID = $id"
+        val query = "delete from $TABLE_ITEMS where $COLUMN_ID = $id"
         try {
             this.writableDatabase.execSQL(query)
         }catch (e:java.lang.RuntimeException){
@@ -57,7 +57,7 @@ class ItemDatabaseHelper(context: Context):SQLiteOpenHelper(context, DB_NAME,nul
         }
     }
     fun getItem(id: Int): Item {
-        val query = "select * from $TABLE_NAME where $COLUMN_ID = $id"
+        val query = "select * from $TABLE_ITEMS where $COLUMN_ID = $id"
         val db = this.readableDatabase
         val cursor = db.rawQuery(query,null)
         cursor.moveToFirst()
@@ -68,7 +68,7 @@ class ItemDatabaseHelper(context: Context):SQLiteOpenHelper(context, DB_NAME,nul
     }
     fun getArrayList():ArrayList<Item>{
         val itemList = mutableListOf<Item>()
-        val query = "select * from $TABLE_NAME"
+        val query = "select * from $TABLE_ITEMS"
         val db = this.readableDatabase
         try {
             val cursor = db.rawQuery(query,null)
